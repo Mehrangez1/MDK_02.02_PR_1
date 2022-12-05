71835ee71a870eea109140b48106656e4990c3c9
Mehrangez :) <113583898+Mehrangez666@users.noreply.github.com>
2022-12-05 11:12:28 +0300

    Add files via upload

---
+++ mdk007/Program.cs
@@ -0,0 +1,17 @@
+int getMax(int a, int b){
+    if(a>b){
+        return b;
+    }
+        return a;
+}
+
+
+            int a = 1;
+            int b = 2;
+            int c = 3;
+            int d = 4;
+
+            int max1= getMax(a, b);
+            int max2= getMax(c, d);
+
+        Console.WriteLine(getMax(max1,max2));
